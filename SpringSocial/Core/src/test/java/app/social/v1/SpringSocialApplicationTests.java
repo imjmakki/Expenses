a1d6f6eeb30968b5e137fae19f11a2ac9c748cf5
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-08 17:13:05 +0300

    first commit for the project backend

---
+++ SpringSocial/Core/src/test/java/app/social/v1/SpringSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package app.social.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
