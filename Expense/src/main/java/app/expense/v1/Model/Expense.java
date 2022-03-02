871ae33557ed7f5dd00f3596aa8043d267157f72
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:19:59 +0300

    adding expenses model

---
+++ Expense/src/main/java/app/expense/v1/Model/Expense.java
@@ -0,0 +1,30 @@
+package app.expense.v1.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "expenses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Expense implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "expense")
+    private String expense;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+}
