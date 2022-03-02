1b76d2a1fbb202f160a1ada43e70731973fa8598
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:32:55 +0300

    updating the rest api configurations

--- Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
+++ Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
@@ -1,9 +1,18 @@
 package app.expense.v1.Controller;
 
+import app.expense.v1.Service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ExpenseAPI {
+
+    private ExpenseService expenseService;
+
+    @Autowired
+    public ExpenseAPI(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
 }
