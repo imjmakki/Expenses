6dcce6f0a655dbca6b2b7585db97e32686552f5f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:29:37 +0300

    adding first implementations

---
+++ Expense/src/main/java/app/expense/v1/Service/Impl/ExpenseServiceImpl.java
@@ -0,0 +1,27 @@
+package app.expense.v1.Service.Impl;
+
+import app.expense.v1.Model.Expense;
+import app.expense.v1.Repo.ExpenseDAO;
+import app.expense.v1.Service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ExpenseServiceImpl implements ExpenseService {
+
+    private ExpenseDAO expenseDAO;
+
+    @Autowired
+    public ExpenseServiceImpl(ExpenseDAO expenseDAO) {
+        this.expenseDAO = expenseDAO;
+    }
+
+    @Override
+    public List<Expense> findAll() {
+        return expenseDAO.findAll();
+    }
+}
