0cc03d1f152ed9ba37e7297de9cb6d621f885531
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:23:14 +0300

    adding list service

--- Expense/src/main/java/app/expense/v1/Service/ExpenseService.java
+++ Expense/src/main/java/app/expense/v1/Service/ExpenseService.java
@@ -1,7 +1,12 @@
 package app.expense.v1.Service;
 
+import app.expense.v1.Model.Expense;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ExpenseService {
+
+    List<Expense> findAll();
 }
