b9942e72598bc6c3169bc2653dd9b29e0d462e41
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:36:00 +0300

    adding first rest api route

--- Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
+++ Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
@@ -1,9 +1,15 @@
 package app.expense.v1.Controller;
 
+import app.expense.v1.Model.Expense;
 import app.expense.v1.Service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +20,11 @@ public class ExpenseAPI {
     @Autowired
     public ExpenseAPI(ExpenseService expenseService) {
         this.expenseService = expenseService;
+    }
+
+    @GetMapping("/expenses")
+    public ResponseEntity<List<Expense>> get() {
+        List<Expense> expenses = expenseService.findAll();
+        return new ResponseEntity<List<Expense>>(expenses, HttpStatus.OK);
     }
 }
