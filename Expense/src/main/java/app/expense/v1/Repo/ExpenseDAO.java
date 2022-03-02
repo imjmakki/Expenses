0141714f5a39b1a6e1463832df3c83f49deb2151
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-02 18:22:28 +0300

    adding service and updating the DAO

--- Expense/src/main/java/app/expense/v1/Repo/ExpenseDAO.java
+++ Expense/src/main/java/app/expense/v1/Repo/ExpenseDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExpensesDAO extends JpaRepository<Expense, Long> {
+public interface ExpenseDAO extends JpaRepository<Expense, Long> {
 }
---
+++ Expense/src/main/java/app/expense/v1/Service/ExpenseService.java
@@ -0,0 +1,7 @@
+package app.expense.v1.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ExpenseService {
+}
