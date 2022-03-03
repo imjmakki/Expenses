11c440f176d95c62d15f46d61f84182cf7db31d3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-03 18:52:07 +0300

    adding the save component

--- expense-angular/src/app/app.module.ts
+++ expense-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { ListExpenseComponent } from './components/list-expense/list-expense.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListExpenseComponent
+    ListExpenseComponent,
+    AddExpenseComponent
   ],
   imports: [
     BrowserModule,
