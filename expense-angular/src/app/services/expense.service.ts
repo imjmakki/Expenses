a174b02e610516389a55fb4bf32c460c636a8b78
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-03 17:35:17 +0300

    adding map service

--- expense-angular/src/app/services/expense.service.ts
+++ expense-angular/src/app/services/expense.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Expense} from "../models/expense";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor() { }
+  private getUrl: string = "http://localhost:8080/api/v1/expenses";
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getExpenses(): Observable<Expense[]> {
+    return this._httpClient.get<Expense[]>(this.getUrl).pipe(
+      map(response => response)
+    )
+  }
 }
