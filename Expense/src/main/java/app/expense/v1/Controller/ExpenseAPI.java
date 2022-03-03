5bfecb84b67aa744189d7b089be4eb5ca0dca92d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-03 17:53:53 +0300

    adding cors origin

--- Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
+++ Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class ExpenseAPI {
 
     private ExpenseService expenseService;
