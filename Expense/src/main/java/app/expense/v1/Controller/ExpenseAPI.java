2e5b6a70987bbb1e486ce5bea341c7c979aaae42
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-03 18:03:26 +0300

    adding expense to the db

--- Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
+++ Expense/src/main/java/app/expense/v1/Controller/ExpenseAPI.java
@@ -5,10 +5,7 @@ import app.expense.v1.Service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,11 @@ public class ExpenseAPI {
     public ResponseEntity<List<Expense>> get() {
         List<Expense> expenses = expenseService.findAll();
         return new ResponseEntity<List<Expense>>(expenses, HttpStatus.OK);
+    }
+
+    @PostMapping("/expenses")
+    public ResponseEntity<Expense> save(@RequestBody Expense expense) {
+        Expense addExpense = expenseService.save(expense);
+        return new ResponseEntity<Expense>(addExpense, HttpStatus.CREATED);
     }
 }
